3bd8ac53aa50b0d17eb0fe18ab66a5eb2357592b
Oneilos <kellernicolas@hotmail.fr>
2015-02-18 18:42:44 +0100

    nettoyage

--- js/simple-sidebar.js
+++ js/simple-sidebar.js
@@ -2,6 +2,8 @@ $("#menu-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
+/* ne marche pas en haut résolution
 $("#sidebar-wrapper a").click(function(e) {
     $("#wrapper").toggleClass("toggled");
 });
+*/
