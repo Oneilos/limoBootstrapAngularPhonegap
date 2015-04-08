3c6b0c076bdfe588f48df220049f35be8c422b2a
Oneilos <kellernicolas@hotmail.fr>
2015-04-08 17:44:12 +0200

    MAJ 08/04/2015

--- js/limo.js
+++ js/limo.js
@@ -1,3 +1,4 @@
+var sound = new Audio("https://www.limo-vtc.fr/nouvelles-demandes.mp3");
 var limoApp = angular.module('limoApp', ['ngRoute']);
 
 limoApp.config(function($routeProvider, $locationProvider) {
@@ -39,9 +40,53 @@ limoApp.config(function($routeProvider, $locationProvider) {
         });
 });
 
+limoApp.filter('replace', function() {
+    return function(input, find, replacement) {
+        return input.replace(find,replacement);
+    }
+});
+limoApp.filter('age', function() {
+    return function(input) {
+        date1 = new Date(input);
+        date2 = new Date();
+        var diff = {}
+        var tmp = date2 - date1
+
+        tmp = Math.floor(tmp/1000);             // Nombre de secondes entre les 2 dates
+        diff.sec = tmp % 60;                    // Extraction du nombre de secondes
+
+        tmp = Math.floor((tmp-diff.sec)/60);    // Nombre de minutes (partie entière)
+        diff.min = tmp % 60;                    // Extraction du nombre de minutes
+
+        tmp = Math.floor((tmp-diff.min)/60);    // Nombre d'heures (entières)
+        diff.hour = tmp % 24;                   // Extraction du nombre d'heures
+
+        tmp = Math.floor((tmp-diff.hour)/24);   // Nombre de jours restants
+        diff.day = tmp;
+
+        if (diff.sec) {
+            diff.string = diff.sec + 'sec';
+        }
+        if (diff.min) {
+            diff.string = diff.min + 'min';
+        }
+        if (diff.hour) {
+            diff.string = diff.hour + 'h';
+        }
+        if (diff.day) {
+            diff.string = diff.day + 'j';
+        }
+
+        return diff.string;
+    }
+});
+
 limoApp.service('AuthService', function () {
     var service = {};
     service.login = function (serveur, login, mdp) {
+        service.serveur = serveur;
+        service.login = login;
+        service.mdp = mdp;
         alert('Connexion: ' + serveur + login + mdp);
         return true ? true : false;
     }
@@ -57,6 +102,28 @@ limoApp.controller('MobilimoCtrl', function ($scope,$http,$interval,$filter) {
 });
 limoApp.controller('LimodriverCtrl', function ($scope,$http,$interval,$filter) {
 });
+limoApp.controller('DemandesCtrl', function ($scope,$http,$interval,$filter) {
+    var active = 0;
+    $scope.setActive = function(index) {
+        active = index;
+    }
+    $scope.isActive = function(index) {
+        return active===index;
+    }
+    update = function(){
+        if (navigator.onLine) {
+            $http.jsonp('https://dev.limovtc.fr/bop3/C_Com_DemandeDevis/jsonp/?DDE_SDD_ID=1&callback=JSON_CALLBACK').
+            success(function(data, status, headers, config) {
+                if (typeof $scope.liste!='undefined' && $scope.liste.length<data.length) {
+                    sound.play();
+                }
+                $scope.liste = data;
+            });
+        }
+    }
+    var timer = setInterval(update, 3000);
+    update();
+});
 limoApp.controller('LoginCtrl', function ($scope,$http,$interval,$filter,AuthService,$location) {
     $scope.login = function(){
         if (AuthService.login($scope.inputServeur, $scope.inputEmail, $scope.inputPassword)) {
@@ -65,4 +132,17 @@ limoApp.controller('LoginCtrl', function ($scope,$http,$interval,$filter,AuthSer
     }
 });
 limoApp.controller('404Ctrl', function ($scope,$http,$interval,$filter) {
+});
+limoApp.controller('StatutCtrl', function($scope,$window) {
+    $scope.online = navigator.onLine;
+    $window.addEventListener("offline", function () {
+        $scope.$apply(function() {
+            $scope.online = navigator.onLine;
+        });
+    }, false);
+    $window.addEventListener("online", function () {
+        $scope.$apply(function() {
+            $scope.online = navigator.onLine;
+        });
+    }, false);
 });
